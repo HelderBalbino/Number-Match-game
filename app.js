d23eb2675634d3fe21ab1805567ce5aa44d07279
Helder Balbino <helderbalbino@googlemail.com>
2021-03-01 22:37:32 +0000

    Added an an array of objects to hold all the pictures

---
+++ app.js
@@ -0,0 +1,18 @@
+const images = [
+	{
+		image_name: 'chickens_4.jpg',
+		number_of_items: 4
+	},
+	{
+		image_name: 'kittens_3.jpg',
+		number_of_items: 3
+	},
+	{
+		image_name: 'macaroons_10.jpg',
+		number_of_items: 10
+	},
+	{
+		image_name: 'pens_3.jpg',
+		number_of_items: 3
+	}
+];
